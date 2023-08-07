bc62e3605c037b9300e10b0ecd56022ec0b0fa44
rahmani djamel <hxhdono11@gmail.com>
2023-08-07 18:41:02 +0200

    new fixes

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -1,13 +1,30 @@
 <template>
-        <section class="max-w-4xl mx-auto text-center mt-16">
-            <div>
-                <img src="@/assets/images/me2.jpg" class="rounded-full w-48 h-48 mx-auto mb-4" alt="" srcset="">
-                <h1 class="text-4xl">
-                    Hey, I'm <span class="text-cyan-500">Rahmani Djamel</span> 
-                </h1>
-                <h2 class="inline-flex mt-2">
-                    Web Devloper   <img src="@/assets/images/lary-head.svg" alt="Head of Lary">
-                </h2>
+      <section class=" dark:bg-gray-900">
+        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+            <div class="mr-auto place-self-center lg:text-start text-center lg:col-span-7 lg:mb-12">
+              <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+                Hey, I'm Rahmani Djamel Abderrahmen a
+                <span  class="animate-text bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-5xl font-black">
+                    Web Developer
+                </span>
+            </h1>
+                            <p class="">
+                 <p class="inline-flex mb-6   font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+                  PHP, Laravel Devloper  <img src="@/assets/images/lary-head.svg" class="mx-3" alt="Head of Lary">
+                 </p>
+                </p>
+                <a href="#" class="animate-bounce inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-cyan-500 hover:bg-dky-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-cyan-900">
+                    Contact Me Now
+                    <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 24 24" fill="#fff" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4H14C17.7712 4 19.6569 4 20.8284 5.17157C22 6.34315 22 8.22876 22 12C22 15.7712 22 17.6569 20.8284 18.8284C19.6569 20 17.7712 20 14 20H10C6.22876 20 4.34315 20 3.17157 18.8284C2 17.6569 2 15.7712 2 12Z" stroke="#1C274C" stroke-width="1.5"></path> <path d="M6 8L8.1589 9.79908C9.99553 11.3296 10.9139 12.0949 12 12.0949C13.0861 12.0949 14.0045 11.3296 15.8411 9.79908L18 8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+                </a>
             </div>
-        </section>
+            <div class="mt-4 lg:mt-0 lg:col-span-5 lg:flex">
+              <div class="image-container">
+                  <img src="@/assets/images/me2.jpg" class="original-image rounded-tr-[200px] rounded-bl-[200px]" alt="mockup">
+                  <img src="@/assets/images/me.jpg" class="hover-image rounded-tr-[200px] rounded-bl-[200px]" alt="other-image">
+              </div>
+          </div>
+                    
+        </div>
+    </section>
 </template>
