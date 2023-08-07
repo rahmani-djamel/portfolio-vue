277c0d680ff1f9b7dbefa182b52a74da3a082b75
rahmani djamel <hxhdono11@gmail.com>
2023-08-07 02:57:08 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import navbar from './components/navbar.vue';
+import hero from './components/hero.vue';
+</script>
+
+<template>
+
+<navbar />
+<hero />
+
+
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/hero.vue
@@ -0,0 +1,13 @@
+<template>
+        <section class="max-w-4xl mx-auto text-center mt-16">
+            <div>
+                <img src="@/assets/images/me2.jpg" class="rounded-full w-48 h-48 mx-auto mb-4" alt="" srcset="">
+                <h1 class="text-4xl">
+                    Hey, I'm <span class="text-cyan-500">Rahmani Djamel</span> 
+                </h1>
+                <h2 class="inline-flex mt-2">
+                    Web Devloper   <img src="@/assets/images/lary-head.svg" alt="Head of Lary">
+                </h2>
+            </div>
+        </section>
+</template>
